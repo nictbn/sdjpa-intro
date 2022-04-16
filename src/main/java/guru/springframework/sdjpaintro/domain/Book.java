d20256ebdacdb80285ba2254f2855012e841ccfe
Nicu Taban <nictbndeveloper@gmail.com>
2022-04-16 17:41:10 +0300

    Adds authorId to book entity using flyway

--- src/main/java/guru/springframework/sdjpaintro/domain/Book.java
+++ src/main/java/guru/springframework/sdjpaintro/domain/Book.java
@@ -16,13 +16,15 @@ public class Book {
     private String title;
     private String isbn;
     private String publisher;
+    private Long authorId;
 
     public Book() {}
 
-    public Book(String title, String isbn, String publisher) {
+    public Book(String title, String isbn, String publisher, Long authorId) {
         this.title = title;
         this.isbn = isbn;
         this.publisher = publisher;
+        this.authorId = authorId;
     }
 
     @Override
@@ -70,5 +72,13 @@ public class Book {
 
     public void setPublisher(String publisher) {
         this.publisher = publisher;
+    }
+
+    public Long getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(Long authorId) {
+        this.authorId = authorId;
     }
 }
